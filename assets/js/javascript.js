239bbbda8d76da356335d91bd612ec678ea2ef84
Stacyui <yuisong710@gmail.com>
2021-06-01 18:35:27 +0900

    Initial Commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,194 @@
+$(document).ready(function () {
+  // header menu
+  $('.nav-toggle').click(function () {
+    $('.mobile-menu').toggleClass('open');
+  });
+  // /header menu
+
+  // home - slider
+  $('.slick-track').slick({
+    infinite: true,
+    speed: 300,
+    fade: true,
+    cssEase: 'linear',
+    autoplay:true,
+    arrows: false
+  });
+
+  // home - slider
+
+  // home - slider bc
+  // $('.home-slide').slick({
+  //   infinite: true,
+  //   speed: 300,
+  //   fade: true,
+  //   cssEase: 'linear',
+  //   autoplay:true,
+  //   arrows: false
+  // });
+
+  // /home - slider bc
+
+  // home - fade in
+  $(window).scroll( function(){
+    // Check the location of the object
+    $('.fade-in').each( function(i){
+        
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        // If the object is inside the window, fade it in!
+        if( bottom_of_window > bottom_of_object ){
+            
+            $(this).animate({'opacity':'1'},500);
+        }
+    }); 
+  });
+  // /home - fade in
+
+  // jeans - shop top
+  setInterval(function(){
+    moveRight();
+  },2800);
+
+  var slideCount = $('.shop-top-images img').length; //count the slides
+  var slideWidth = $('.shop-top-images img').width();
+  var slideHeight = $('.shop-top-images img').height();
+  
+  var slideUlWidth = slideCount * slideWidth;
+
+  $('.shop-top-slider').css({ width: slideWidth, height: slideHeight });
+  $('.shop-top-images').css({width:slideUlWidth, marginLeft: -slideWidth});
+
+  $('shop-top-images img:last-child').prependTo('.shop-top-images');
+
+  function moveLeft(){
+    $('.shop-top-images').animate({
+      left: + slideWidth
+    },200, function(){
+      $('.shop-top-images img:last-child').prependTo('.shop-top-images');
+      $('.shop-top-images').css('left', '');
+    });
+  }
+
+  function moveRight(){
+    $('.shop-top-images').animate({
+      left: - slideWidth
+    },200, function(){
+      $('.shop-top-images img:first-child').appendTo('.shop-top-images');
+      $('.shop-top-images').css('left', '');
+    });
+  }
+  // / jeans - shop top
+
+  // item women o men
+  $('#allItems').click(function(){
+    $('.one-item-mens').show(),
+    $('.one-item-womens').show();
+  });
+
+  $('#womensItems').click(function(){
+    $('.one-item-mens').hide(),
+    $('.one-item-womens').show();
+  });
+
+  $('#mensItems').click(function(){
+    $('.one-item-womens').hide(),
+    $('.one-item-mens').show();
+  });
+  // /item women o men
+
+  // faq - sliders
+  $('#flip-a').click(function () {
+    $('#panel-a').slideToggle();
+  });
+
+  $('#flip-b').click(function () {
+    $('#panel-b').slideToggle();
+  });
+
+  $('#flip-c').click(function () {
+    $('#panel-c').slideToggle();
+  });
+
+  $('#flip-d').click(function () {
+    $('#panel-d').slideToggle();
+  });
+  // /faq - sliders
+
+  // contact - click
+  $('#contact').click(function () {
+    $(".contact-us").show();
+    $('.faq-menu').animate({ top: '500px' }, 'slow').css({ display: 'none' });
+    $('.s-locations').css({ display: 'none' }),
+    $('.order-history').css({ display: 'none' });
+  });
+  // /contact - click
+
+  // faq - click
+  $('#faq').click(function () {
+    $(".contact-us").hide();
+    $('.faq-menu').css({ display: 'block' }).animate({ top: '-500px' });
+    $('.s-locations').css({ display: 'none' }),
+    $('.order-history').css({ display: 'none' });
+  });
+
+  $('#order-history-infaq').click(function () {
+    $('.order-history').css({ display: 'block' }),
+    $('.faq-menu').css({ display: 'none'});
+  });
+  // /faq - click
+
+  // store locations - click
+  $('#store-locations').click(function () {
+    $('.contact-us').hide();
+    $('.faq-menu').css({ display: 'none' });
+    $('.s-locations').css({ display: 'block' }).animate({ right: '500px' }),
+    $('.order-history').css({ display: 'none' });
+  });
+  // /store locations
+
+  // order history
+  $('#order-history').click(function () {
+    $('.contact-us').hide();
+    $('.faq-menu').css({ display: 'none' });
+    $('.s-locations').css({ display: 'none' }).animate({ right: '500px' }),
+    $('.order-history').css({ display: 'block' });
+  });
+  // /order history
+
+  // keith count stock
+  var stockVal = 100;
+    var cart = 0;
+
+    $('.add').click(function(){
+        cart = cart + 1;
+        $('.cart').val(cart);
+
+        stockVal = stockVal - 1;
+        $('.stockLeft').val(stockVal);
+        
+    });
+
+
+    $('.minus').click(function(){
+        
+        if (cart == 0){
+            cart = 0;
+        }else{
+            cart = cart - 1;
+            $('.cart').val(cart);
+            stockVal = stockVal + 1;
+           $('.stockLeft').val(stockVal);
+        }
+    });
+    // /keith count stock
+
+    // keith fit details
+
+    $('#fit-details').click(function(){
+      $('#fit-details-panel').toggle();
+    });
+    // /keith fit details
+
+});
